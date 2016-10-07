225f0951ceb150fde050588dedae22599632f0cf
Gabriel Pitali <gabrielpituca@gmail.com>
2016-10-06 21:32:46 -0300

    Add Readme and Script.

---
+++ README.md
@@ -0,0 +1,31 @@
+# Ubuntu post install bash script
+
+This is a simple bash script to run after a fresh Ubuntu GNOME install. It will add/remove some PPAs and packages.
+
+## Installed packages:
+- Chromium
+- Git
+- Gnome tweak tool
+- KeePass 2
+- Spotify (with PPA)
+- Steam
+- Sublime Text 3
+- Tor Browser (with PPA)
+- Ubuntu restricted addons/extras
+- VirtualBox
+- VLC
+
+## Removed packages:
+- Aisleriot Solitaire
+- Brasero
+- Empathy
+- Evolution
+- Mahjongg
+- Mines
+- Sudoku
+
+## Usage:
+- Clone or download this repo
+- `bash ubuntu-post-install-script.sh`
+
+Based on [rougeth - ubuntu-post-install.sh](https://gist.github.com/rougeth/8108714)
---
+++ ubuntu-post-install-script.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+echo '------------------------------------------------------------------------'
+echo '=> Ubuntu GNOME 16.04 post-install script'
+echo '------------------------------------------------------------------------'
+
+# -----------------------------------------------------------------------------
+# => Remove unused packages
+# -----------------------------------------------------------------------------
+echo '=> Remove unused'
+sudo apt purge -y aisleriot brasero empathy evolution gnome-mahjongg gnome-mines gnome-sudoku
+sudo apt autoremove -y
+
+echo 'Done.'
+
+# -----------------------------------------------------------------------------
+# => Add PPAs (Personal Package Archives)
+# -----------------------------------------------------------------------------
+echo '=> Add PPAs'
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-3 # Sublime Text 3
+sudo add-apt-repository ppa:upubuntu-com/tor-bundle -y # tor
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 # spotify
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list # spotify
+
+echo 'Done.'
+
+# -----------------------------------------------------------------------------
+# => System update/upgrade
+# -----------------------------------------------------------------------------
+echo '=> Update repository information'
+sudo apt-get update -qqtr
+echo '=> Performe system upgrade'
+sudo apt-get dist-upgrade -y
+echo '=> Performe apps upgrade'
+sudo apt-get upgrade -y
+echo 'Done.'
+
+# -----------------------------------------------------------------------------
+# => Install apps
+# -----------------------------------------------------------------------------
+echo '=> Install apps'
+sudo apt-get install -y chromium-browser \
+git \
+gnome-tweak-tool \
+keepass2 \
+spotify-client \
+steam \
+sublime-text-installer \
+tor \
+ubuntu-restricted-addons \
+ubuntu-restricted-extras \
+virtualbox \
+vlc
+
+echo 'Done.'
